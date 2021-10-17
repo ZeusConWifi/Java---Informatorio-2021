74d1693b01e46eea9a5aba646d9099b1d75e39c9
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-10-16 21:04:28 -0300

    Add files via upload

---
+++ EjerciciosComplementariosLevel1/Cinco.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Cinco {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.print("Ingrese el primero número entero: ");
+        int m = num.nextInt();
+        System.out.print("Ingrese el segundo número entero: ");
+        int n = num.nextInt();
+        int mult = mult(m, n);
+        System.out.println("La multiplicación sucesiva de ambos números es= "+ mult);
+    }
+    static int mult(int m, int n){
+        int mul = 0;
+        if((m != 0) && (n != 0)){
+            for (int i = 0; i < n; i++) {
+                mul += m;
+            }
+        }
+        return mul;
+    }
+}
