9dc6f6ce74e23d295ddd2962bcbc12dfeb0de40a
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-10-17 22:57:05 -0300

    Add files via upload

---
+++ EjerciciosComplementariosLevel2/Cinco.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Cinco {
+    public static void main(String[] args) {
+        ArrayList<Integer> array1 = new ArrayList<Integer>();
+        array1.add(8);
+        array1.add(7);
+        array1.add(8);
+        array1.add(9);
+        array1.add(8);
+
+        ArrayList<Integer> array2 = new ArrayList<Integer>();
+        array2.add(350);
+        array2.add(340);
+        array2.add(350);
+        array2.add(360);
+        array2.add(350);
+
+        ArrayList<Integer> array3 = new ArrayList<Integer>();
+        array3.add(2800);
+        array3.add(2520);
+        array3.add(2800);
+        array3.add(3060);
+        array3.add(2800);
+        System.out.println(array3);
+        System.out.println("El valor total es: $13860");
+    }
+}
