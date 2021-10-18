ddb0c8bbc073b689e949585067e6ea5784682e98
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-10-18 13:45:33 -0300

    Update Cinco.java

--- EjerciciosComplementariosLevel2/Cinco.java
+++ EjerciciosComplementariosLevel2/Cinco.java
@@ -2,56 +2,40 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Cinco {
     public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
-        ArrayList<Integer> horasTrabajadas = new ArrayList<Integer>();
-        
-        ArrayList<Integer> valorHora = new ArrayList<Integer>();
-        
-        ArrayList<Integer> totalDia = new ArrayList<Integer>();
-        
-        int valor = 0;
-        
-        int sumaFinal = 0;
-        
+        Scanner scan = new Scanner(System.in);       
+        ArrayList<Integer> horasTrabajadas = new ArrayList<Integer>();       
+        ArrayList<Integer> valorHora = new ArrayList<Integer>();       
+        ArrayList<Integer> totalDia = new ArrayList<Integer>();        
+        int valor = 0;       
+        int sumaFinal = 0;       
         int dias = 1;
 
-        while (dias < 6){
-            
-            System.out.println("¿Cuantas horas se trabajó el dia # " + dias +"?");
-            
-            valor = scan.nextInt();
-            
+        while (dias < 6){            
+            System.out.println("¿Cuantas horas se trabajó el dia #" + dias +"?");           
+            valor = scan.nextInt();           
             horasTrabajadas.add(valor);
-
-            System.out.println("¿Cuanto vale cada hora trabajada del dia # " + dias +"?");
-            
-            valor = teclado.nextInt();
-            
-            valorHora.add(valor);
-           
+            System.out.println("¿Cuanto vale cada hora trabajada del dia #" + dias +"?");           
+            valor = scan.nextInt();           
+            valorHora.add(valor);        
             dias++;
         }
         scan.close();
 
-        for (int dia = 0; dia < valorHora.size(); dia++) {
-            
-            valor = valorHora.get(dia) * horasTrabajadas.get(dia);
-            
+        for (int dia = 0; dia < valorHora.size(); dia++) {     
+            valor = valorHora.get(dia) * horasTrabajadas.get(dia);     
             totalDia.add(valor);
         }
 
-        for (int i :totalDia){
-            
+        for (int i :totalDia){         
             sumaFinal += i;
         }
 
-        System.out.println("El monto a cobrar por dia será de: " + totalDia);
-        
+        System.out.println("El monto a cobrar por dia será de: " + totalDia);      
         System.out.println("El monto semanal será de: " + sumaFinal);
-
     }
 }
