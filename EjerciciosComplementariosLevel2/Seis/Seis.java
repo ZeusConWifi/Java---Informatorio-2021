e835db2bcbf6ee1c8c8fba4854acf8b28e5c8ec7
ZeusConWifi <89951133+ZeusConWifi@users.noreply.github.com>
2021-10-18 13:21:55 -0300

    Create Seis.java

---
+++ EjerciciosComplementariosLevel2/Seis/Seis.java
@@ -0,0 +1,35 @@
+package Seis;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Seis {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> Diccionario;
+        
+        Diccionario = new HashMap<>();
+
+        Set<Empleado> lista ; 
+        
+        lista = new HashSet<>(); 
+        
+        lista.add(new Empleado("Leonel", "Messi", 10101010, 8, 352));
+        
+        lista.add(new Empleado("Rodrigo", "De Paul", 07070707, 7, 310));
+        
+        lista.add(new Empleado("Marcos", "Acuña", 11111111, 8, 456));
+        
+        lista.add(new Empleado("Lautaro", "Martínez", 09090909, 9, 440));
+
+        for (Empleado emp : lista) {
+            
+            int sueldo = emp.calcularSueldo();
+            
+            Diccionario.put(emp.dni, sueldo);
+        }
+        System.out.print(Diccionario);
+    }
+}
